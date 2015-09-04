cbe84be0fa73789526044de638039fdfa3b52bbc
Scott Persinger <scottpersinger@gmail.com>
2015-09-04 16:24:16 -0700

    Added preath and setup commands

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 exports.topics = [{
   name: 'connect',
   description: 'manage connections for heroku connect'
@@ -5,6 +6,8 @@ exports.topics = [{
 
 exports.commands = [
   require('./lib/commands/connect/status'),
+  require('./lib/commands/connect/preauth'),
+  require('./lib/commands/connect/setup'),
   require('./lib/commands/connect/create-mapping'),
   require('./lib/commands/connect/delete-mapping')
 ];
