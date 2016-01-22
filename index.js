efbff292ab3620bc8d89215511b7b90e2c45cb2d
Marty Alchin <marty@martyalchin.com>
2016-01-22 11:12:56 -0500

    Add a resume command

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ exports.commands = [
   require('./lib/commands/connect/import'),
   require('./lib/commands/connect/export'),
   require('./lib/commands/connect/pause'),
+  require('./lib/commands/connect/resume'),
   require('./lib/commands/connect/create-mapping'),
   require('./lib/commands/connect/delete-mapping')
 ];
