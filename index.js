cf8de0011e501b84deeb509e2fb37c18a2cb942e
Marty Alchin <marty@heroku.com>
2016-02-04 15:07:05 -0500

    Add a command to get a JSON view of a connection

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ exports.topics = [{
 exports.commands = [
   require('./lib/commands/connect/info'),
   require('./lib/commands/connect/state'),
+  require('./lib/commands/connect/json'),
   require('./lib/commands/connect/preauth'),
   require('./lib/commands/connect/auth_connection'),
   require('./lib/commands/connect/setup'),
