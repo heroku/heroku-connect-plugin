eee8595d178687f36618f97415c5ac2c4e0bfbe9
Ian Cordasco <graffatcolmingov@gmail.com>
2017-05-01 08:35:23 -0500

    Add test for heroku connect:sate

--- test/commands/connect/state.js
+++ test/commands/connect/state.js
@@ -1,8 +1,10 @@
 'use strict'
+/* globals describe beforeEach it */
+
 const cli = require('heroku-cli-util')
 const nock = require('nock')
-const unexpected = require('unexpected')
-const cmd = require('../../../lib/commands/connect/state')
+const expect = require('unexpected')
+const stateCmd = require('../../../lib/commands/connect/state')
 
 describe('connect:state', () => {
   // prevent stdout/stderr from displaying
@@ -10,7 +12,32 @@ describe('connect:state', () => {
   beforeEach(() => cli.mockConsole())
 
   it('retrieves the state of the connect addon', () => {
-    let api = nock('https://connect-us.heroku.com:443')
-      //.get
+    let appName = 'fake-app'
+    let resourceId = 'abcd-ef01'
+    let password = 's3cr3t3'
+    let api = nock('https://connect-us.heroku.com:443', {
+      reqheaders: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${password}`,
+        'heroku-client': 'toolbelt'
+      }
+    })
+      .get('/api/v3/connections')
+      .query({deep: true, app: appName, resource_id: resourceId})
+      .reply(200, {results: [{state: 'NEW'}]})
+
+    return stateCmd.run({
+      app: appName,
+      flags: {
+        resource_id: resourceId,
+        region: 'us'
+      },
+      auth: {
+        password: password
+      }
+    }, {})
+      .then(() => expect(cli.stdout, 'to equal', 'NEW\n'))
+      .then(() => expect(cli.stderr, 'to be empty'))
+      .then(() => api.done())
   })
 })
