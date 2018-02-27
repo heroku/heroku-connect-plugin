ac6c1b11be6b18e0215c62f4b8c965c18671bf2e
Ian Stapleton Cordasco <icordasco@heroku.com>
2018-02-27 10:11:44 -0600

    Fix style and test errors
    If there's a single resource provided, only show the state of that
    single connection.

--- commands/connect/state.js
+++ commands/connect/state.js
@@ -8,16 +8,23 @@ function * run (context, heroku) {
   context.region = yield regions.determineRegion(context, heroku)
   let connections = yield api.withUserConnections(context, context.app, context.flags, heroku)
 
-  if (context.flags.json) {
-    cli.styledJSON(connections)
+  if (!context.flags.resource) {
+    if (context.flags.json) {
+      cli.styledJSON(connections)
+    } else {
+      cli.table(connections, {
+        columns: [
+          {key: 'app_name', label: 'App'},
+          {key: 'db_key', label: 'Database'},
+          {key: 'schema_name', label: 'Schema'},
+          {key: 'state', label: 'State'}
+        ]
+      })
+    }
   } else {
-    cli.table(connections, {
-      columns: [
-        {key: 'app_name', label: 'App'},
-        {key: 'db_key', label: 'Database'},
-        {key: 'schema_name', label: 'Schema'},
-        {key: 'state', label: 'State'},
-      ],
+    // Retrieving a single resource
+    connections.forEach(function (connection) {
+      cli.log(connection.state)
     })
   }
 }
