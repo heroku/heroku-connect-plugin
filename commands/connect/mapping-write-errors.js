4b9c41cad4862a97cf3c1d6efa26a8fafcd68376
Ian Stapleton Cordasco <icordasco@heroku.com>
2018-02-28 10:05:04 -0600

    Use heroku-pg to query write errors from the DB
    This depends on heroku/heroku-pg#188 for it to work properly. Otherwise,
    this now shows both _trigger_log and _trigger_log_archive failed writes.

--- commands/connect/mapping-write-errors.js
+++ commands/connect/mapping-write-errors.js
@@ -1,37 +1,36 @@
 'use strict'
 const api = require('../../lib/connect/api.js')
 const regions = require('../../lib/connect/regions.js')
+const pg = require('heroku-pg')
 const cli = require('heroku-cli-util')
 const co = require('co')
 
 function * run (context, heroku) {
   context.region = yield regions.determineRegion(context, heroku)
-  let mappingName = context.args.name
+  let mappingName = context.args.name.toLowerCase()
   let connection = yield api.withConnection(context, heroku)
-  let mapping = yield api.withMapping(connection, mappingName)
-  let results = yield cli.action('Retrieving errors', co(function * () {
-    let url = '/api/v3/mappings/' + mapping.id + '/errors'
-    return yield api.request(context, 'GET', url)
-  }))
-  let errors = results.json
+  let query = `
+SELECT
+  id, processed_at, record_id, sf_message, false AS archived
+FROM
+  ${connection.schema_name}._trigger_log
+WHERE
+  state = 'FAILED' AND table_name = '${mappingName}'
+UNION
+SELECT
+  id, processed_at, record_id, sf_message, true AS archived
+FROM
+  ${connection.schema_name}._trigger_log_archive
+WHERE
+  state = 'FAILED' AND table_name = '${mappingName}'
+ORDER BY
+  processed_at DESC
+`
+  let fetcher = pg.fetcher(heroku)
+  let db = yield fetcher.database(context.app, connection.db_key)
 
-  if (errors.count === 0) {
-    cli.log('No write errors in the last 24 hours')
-  } else {
-    if (context.flags.json) {
-      cli.styledJSON(errors.results)
-    } else {
-      cli.table(errors.results, {
-        columns: [
-          {key: 'id', label: 'Trigger Log ID'},
-          {key: 'table_name', label: 'Table Name'},
-          {key: 'record_id', label: 'Table ID'},
-          {key: 'message', label: 'Error Message'},
-          {key: 'created_at', label: 'Created'}
-        ]
-      })
-    }
-  }
+  let output = yield pg.psql.exec(db, query)
+  cli.log(output)
 }
 
 module.exports = {
@@ -50,11 +49,6 @@ module.exports = {
       name: 'resource',
       description: 'specific connection resource name',
       hasValue: true
-    },
-    {
-      name: 'json',
-      description: 'print errors as styled JSON',
-      hasValue: false
     },
     regions.flag
   ],
