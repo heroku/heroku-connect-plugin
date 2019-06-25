40baa04517b5263372d6740cb514b075d97f50ae
Chris Lambacher <clambacher@heroku.com>
2019-06-25 11:30:46 -0400

    Add back ability to check for access to addons without addons:open

--- commands/connect-events/info.js
+++ commands/connect-events/info.js
@@ -10,12 +10,22 @@ module.exports = {
   description: 'display connection information',
   help: 'display connection information',
   flags: [
-    {name: 'resource', description: 'specific connection resource name', hasValue: true}
+    {name: 'resource', description: 'specific connection resource name', hasValue: true},
+    {name: 'check-for-new', char: 'c', description: 'check for access to any new connections', hasValue: false}
   ],
   needsApp: true,
   needsAuth: true,
   run: cli.command(co.wrap(function * (context, heroku) {
-    let connections = yield api.withUserConnections(context, context.app, context.flags, true, heroku, api.ADDON_TYPE_EVENTS)
+    let connections
+
+    if (context.flags['check-for-new']) {
+      connections = yield api.requestAppAccess(context, context.app, context.flags, true, heroku, api.ADDON_TYPE_EVENTS)
+    } else {
+      connections = yield api.withUserConnections(context, context.app, context.flags, true, heroku, api.ADDON_TYPE_EVENTS)
+      if (connections.length === 0) {
+        connections = yield api.requestAppAcess(context, context.app, context.flags, true, heroku, api.ADDON_TYPE_EVENTS)
+      }
+    }
 
     if (connections.length === 0) {
       const instanceName = process.env['CONNECT_ADDON'] === 'connectqa' ? 'platformeventsqa' : 'herokuconnect'
