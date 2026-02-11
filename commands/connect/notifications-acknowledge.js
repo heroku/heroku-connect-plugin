2dea230c86d6657de7754a11a175e94f401b71e0
Marcel Santos <83613780+mhsantos-sfdc@users.noreply.github.com>
2026-02-11 13:53:17 -0300

    feat: add a notifications command to list and ack notifications (#247)

---
+++ commands/connect/notifications-acknowledge.js
@@ -0,0 +1,35 @@
+'use strict'
+const api = require('../../lib/connect/api.js')
+const cli = require('@heroku/heroku-cli-util')
+const co = require('co')
+
+module.exports = {
+  topic: 'connect:notifications',
+  command: 'acknowledge',
+  description: 'Acknowledges notifications matching the given criteria',
+  help: 'Finds and acknowledges notifications matching the given criteria',
+  flags: [
+    { name: 'after', description: 'start date for notifications', hasValue: true },
+    { name: 'before', description: 'end date for notifications', hasValue: true },
+    { name: 'event-type', description: 'type of event to filter by', hasValue: true },
+    { name: 'resource', description: 'specific connection resource name', hasValue: true }
+  ],
+  needsApp: true,
+  needsAuth: true,
+  run: cli.command(co.wrap(function * (context, heroku) {
+    const connection = yield api.withConnection(context, heroku)
+    context.region = connection.region_url
+
+    const params = {
+      page_size: 1000,
+      after: context.flags.after,
+      before: context.flags.before,
+      event_type: context.flags['event-type']
+    }
+
+    const response = yield api.request(context, 'POST', '/api/v3/connections/' + connection.id + '/notifications/acknowledge', null, params)
+    if (response.status !== 204) {
+      throw new Error(response.data.message || 'unknown error')
+    }
+  }))
+}
